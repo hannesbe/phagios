085c7732faf5b8bcc186e0c2afc90e5a58db7b95
patyx7 <code@introspect.in>
2013-11-07 23:27:53 +0000

    Updated README, removed namespaces, made script PHP5 compatible.

--- README.md
+++ README.md
@@ -1,36 +1,33 @@
-README
+Phagios
 ======
 
-Phagios - PHP 5.3+ Nagios Plugin Helper
-=======
-
-Phagios is work-in-progress PHP 5.3+ helper for building Nagios plugins using PHP.
-Being an alpha it is not recommended for use in production environments.
+Phagios is helper library for building Nagios plugins using PHP.
+It is work-in-progress alpha and is not recommended for use in production environments.
 
 Requirements
 ------------
 
-- PHP 5+
+- PHP 5
 
-Installation
+Installation and Usage
 ------------
 
+For now installing can be done by cloning the repository
 ```  
 git clone git://github.com/patyx7/phagios.git
 ```
+and then including helper library at the top of your plugin file.
+```  
+$phagios = require './phagios.php';
+```
 
-Usage
+Contribute and Feedback
 ------------
 
-Coming soon...
-
-Contributing
-------------
-
-Feel free to fork the project, create a feature branch, and send me a pull request.  
-If you would like to help please take a look at the [list of issues](http://github.com/patyx7/phagios/issues).
+Please submit issues and send your feedback and suggestions as often as you have them.
+Also feel free to fork the project, create a feature branch, and send me a pull request and I will review it.
 
 License
 -------
 
-Phagios is licensed under the GNU GENERAL PUBLIC LICENSE - see the LICENSE file for details.
+Phagios is licensed under the GNU GENERAL PUBLIC LICENSE - see the [LICENSE.md](LICENSE.md) file for details.
--- source/Phagios.php
+++ source/Phagios.php
@@ -1,9 +1,10 @@
+#!/usr/bin/php
 <?php
 
 /* vim: set expandtab tabstop=4 shiftwidth=4 softtabstop=4: */
 
 /**
- *   Phagios - PHP 5.3+ Nagios Plugin Helper
+ *   Phagios - PHP 5 Nagios Plugin Helper
  *   Copyright (C) 2013 Patrick Kuti
  *
  *   This program is free software: you can redistribute it and/or modify
@@ -19,14 +20,19 @@
  *   You should have received a copy of the GNU General Public License
  *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
- * @author    Patrick Kuti <code@introspect.in>
- * @copyright 2013 Patrick Kuti
- * @license   http://www.gnu.org/licenses/gpl-3.0-standalone.html GNU General Public License
- * @version   Release: 0.0.1a
- * @link      http://github.com/patyx7/phagios
+ * @package     patyx7 / phagios 
+ * @copyright   Copyright (c) 2013, Patrick Kuti
+ * @license     http://www.gnu.org/licenses/gpl-3.0-standalone.html GNU General Public License
+ * @link        http://github.com/patyx7/phagios
+ * @author      Patrick Kuti <code@introspect.in>
  */
 
-namespace Phagios;
+date_default_timezone_set('UTC');
+define("VERSION", '1.0.0');
+
+ini_set('max_execution_time', '55');
+ini_set('display_errors', true);
+ini_set('display_startup_errors', true);
 
 /**
  * Main Phagios Class
@@ -36,91 +42,163 @@ abstract class Phagios
 {
 
     /**
-     * Constant for Nagios unknown
+     * Nagios plugin return code of Unknown
+     * 
+     * @var integer
      */
-    const UNKNOWN = 3;
+    const STATE_UNKNOWN = 3;
 
     /**
-     * Constant for Nagios critical
+     * Nagios plugin return code of Critical
+     * 
+     * @var integer
      */
-    const CRITICAL = 2;
+    const STATE_CRITICAL = 2;
 
     /**
-     * Constant for Nagios warning
+     * Nagios plugin return code of Warning
+     * 
+     * @var integer
      */
-    const WARNING = 1;
+    const STATE_WARNING = 1;
 
     /**
-     * Constant for Nagios ok
+     * Nagios plugin return code of OK
+     * 
+     * @var integer
      */
-    const OK = 0;
+    const STATE_OK = 0;
 
     /**
-     * Name of the plugin
+     * Nagios verbosity level of summary (default)
+     * 
+     * @var integer
+     */
+    const VERBOSE_SUMMARY = 0;
+
+    /**
+     * Nagios verbosity level of additional information
+     * 
+     * @var integer
+     */
+    const VERBOSE_ADDITIONAL = 1;
+
+    /**
+     * Nagios verbosity level of debug outout
+     * 
+     * @var integer
+     */
+    const VERBOSE_DEBUG = 2;
+
+    /**
+     * Nagios verbosity level of problem diagnosis
+     * 
+     * @var integer
+     */
+    const VERBOSE_DIAGNOSIS = 3;
+
+    /**
+     * Name of the plugin that will be run
+     *
+     * @var string
      */
     protected $pluginName = 'Phagios';
 
     /**
-     * Version of the plugin
+     * Version of the plugin that will be run
+     *
+     * @var string
      */
     protected $pluginVersion = '0.0.1a';
 
     /**
-     * Description of the plugin
+     * Description of the plugin that will be run
+     *
+     * @var string
      */
     protected $pluginDescription = 'Nagios Plugin Helper for PHP';
 
     /**
-     * Example usage of the plugin
+     * Example usage of the plugin that will be run
+     *
+     * @var string
      */
-    protected $pluginUsage = './phagios.php -H <host_address> -p <port> -U <username> -P <password> -t <timeout> -w <warning> -c <critical>';
+    protected $pluginUsage = "./phagios.php -V -h -H <host_address> -p <port> -U <username> -P <password> -t <timeout> -w <warning_threshold> -c <critical_threshold>";
 
     /**
-     * Verbosity level
+     * Verbosity level for verbose output
+     * 
+     * @var integer
      */
     protected $verbosity = 0;
 
     /**
-     * Set the verbosity of the plugin
+     * Sets the verbosity level for the plugin that is about to run
+     * 
+     * @param type $level 
+     * 
+     * @return type
      */
     public function setVerbosity($level)
     {
+        if ($level > 3 || $level < 0) {
+            throw new Exception('Verbosity can only be set to an integer between and including 0 to 3.');
+        }
         $this->verbosity = $level;
     }
 
     /**
      * Main method to execute plugin's run method
+     * 
+     * @method main
+     * 
+     * @return null
      */
     public function main()
     {
         try {
             $pluginResult = $this->run();
-        } catch (\Phagios\Exception\Unknown $e) {
-            // Bail out with information and prefData
-        } catch (\Phagios\Exception\Critcal $e) {
-            // Bail out with information and prefData
-        } catch (\Phagios\Exception\Warning $e) {
-            // Bail out with information and prefData
-        } catch (\Exception $e) {
-            // Generic warning can go here
+        } catch (PhagiosUnknownException $e) {
+            $this->cleanExit(self::STATE_UNKNOWN, $e->getMessage());
+        } catch (PhagiosCritcalException $e) {
+            $this->cleanExit(self::STATE_CRITICAL, $e->getMessage());
+        } catch (PhagiosWarningException $e) {
+            $this->cleanExit(self::STATE_WARNING, $e->getMessage());
+        } catch (Exception $e) {
+            $this->cleanExit(self::STATE_UNKNOWN, $e->getMessage());
         }
 
-        print $pluginResult;
+        $this->cleanExit(self::STATE_OK, $pluginResult);
     }
 
     /**
      * Run the plugin
      */
     abstract protected function run();
+
+    /**
+     * Exit gracefully with correct return codes
+     *
+     * @method cleanExit
+     *
+     * @param  integer    $state   [description]
+     * @param  string    $message [description]
+     *
+     * @return null  
+     */
+    private function cleanExit($state, $message)
+    {
+        print($message);
+        exit($state);
+    }
 }
 
-
-class UnknownException extends \Exception
+class PhagiosUnknownException extends Exception
 {
 }
-class CrticalException extends \Exception
+class PhagiosCritcalException extends Exception
 {
 }
-class WarningException extends \Exception
+class PhagiosWarningException extends Exception
 {
 }
